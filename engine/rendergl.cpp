d69c558bf72f65e6a68ef51293594bf65ecf370e
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-09-16 00:46:04 +0200

    do not use menufps, rely on vsync

--- engine/rendergl.cpp
+++ engine/rendergl.cpp
@@ -181,7 +181,7 @@ VAR(rtscissor, 0, 1, 1);
 VAR(blurtile, 0, 1, 1);
 VAR(rtsharefb, 0, 1, 1);
 VAR(vsync, 0, 1, 2);
-extern int maxfps, menufps;
+extern int maxfps;
 
 static bool checkseries(const char *s, int low, int high)
 {
@@ -2631,11 +2631,6 @@ ullong draw(){
         if(mainmenu){
             SDL_GL_SetSwapInterval(1);
             SDL_GL_SwapWindow(screen);
-            if(menufps){
-                ullong now = tick();
-                ullong next = start + 1000000000ULL/menufps;
-                return next > now ? next - now : 0;
-            }
             return 0;
         }
         if(!vsync){
